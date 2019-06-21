228c8facd4db93f6b711ece0bf5cfeb3c6ec2796
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-06-21 18:54:59 +0200

    Add Wallet

---
+++ js/Wallet.js
@@ -0,0 +1,26 @@
+class Wallet {
+  constructor(money) {
+    let _money = money;
+
+    this.getWalletValue = () => _money;
+
+    this.checkCanPlay = value => {
+      if (money >= value) return true;
+      return false;
+    };
+
+    this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return (_money += value);
+        } else if (type === "-") {
+          return (_money -= value);
+        } else {
+          throw new Error("Nieprawidłowy typ działania");
+        }
+      } else {
+        throw new Error("Nieprawidłowa liczba");
+      }
+    };
+  }
+}
